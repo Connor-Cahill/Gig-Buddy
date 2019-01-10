9e861d75088e492b45ad1ac2a5d4253828813498
Connor Cahill <connorcahill913@gmail.com>
2019-01-09 22:08:14 -0800

    fixed logic for styling on clients-show. added services to dashboard.Also you can now properly add services to a client

--- controllers/clients.js
+++ controllers/clients.js
@@ -1,23 +1,26 @@
 const Client = require('../models/client');
 const wrap = require('../middleware/errorHandler');
+const Service = require('../models/service')
 
 module.exports = function(app) {
 
     // // GET: returns list of all clients
     app.get('/clients', wrap( async (req, res) => {
-        const clients = await Client.find({}).exec();
+        const clients = await Client.find({}).populate('services').exec();
         res.render('clients-index', { clients });
     }));
 
-    //  GET: renders the clients form 
-    app.get('/clients/new', (req, res) => {
-        res.render('client-form');
-    })
+    //  GET: renders the clients form and sends service data 
+    app.get('/clients/new', wrap(async (req, res) => {
+        const services = await Service.find({});
+        res.render('client-form', { services });
+    }))
 
     //  GET: returns single client given the ID 
     app.get('/clients/:id', wrap( async (req, res) => {
-        const client = await Client.findOne({ _id: req.params.id }).exec();
-        res.render('clients-show', { client });
+        const client = await Client.findOne({ _id: req.params.id }).populate('services').exec();
+        const services = await Service.find({}).exec();
+        res.render('clients-show', { client, services });
     }))
 
     
@@ -41,6 +44,22 @@ module.exports = function(app) {
     app.delete('/clients/:id', wrap(async (req, res) => {
         await Client.findOneAndRemove({ _id: req.params.id }).exec();
         res.redirect('/');
+    }));
+
+    //  PATCH: adds services to client 
+    app.put('/client/:id', wrap(async (req, res) => {
+        const client = await Client.findOne({ _id: req.params.id }).exec();
+        client.services.push(req.body)
+        await client.save();
+        res.redirect(`/clients/${client._id}`);
+    }));
+
+    // PATCH: remove a service from a client
+    app.put('/clients/:id/service/:serviceId', wrap(async (req, res) => {
+        const client = await Client.findOne({ _id: req.params.id }).exec();
+        const service = await Service.findOne({ _id: req.params.serviceId }).exec();
+        client.services.pop(indexOf(service));
     }))
+
 
 }
--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -13,3 +13,29 @@ formBtn.addEventListener('click', () => {
     dropDownForm.classList.toggle('showing');
     formBtn.classList.toggle('btn-in-use');
 })
+
+
+//  Function to toggle main Nav buttons color/effect
+const mainNavBtns = document.querySelectorAll('.main-nav-btn');
+console.log(mainNavBtns)
+const clientBtn = document.getElementById('btn-a');
+const servicesBtn = document.getElementById('btn-b');
+const paymentsBtn = document.getElementById('btn-c');
+const sel = 'selected'
+// mainNavBtns.forEach(function(btn) {
+//     btn.addEventListener('click', () => {
+//         btn.classList.add(sel);
+//         if (btn === clientBtn) {
+//             paymentsBtn.classList.remove(sel);
+//             servicesBtn.classList.remove(sel);
+//         } else if (btn === paymentsBtn) {
+//             servicesBtn.classList.remove(sel);
+//             clientBtn.classList.remove(sel);
+//         } else if (btn === servicesBtn) {
+//             paymentsBtn.classList.remove(sel);
+//             clientBtn.classList.remove(sel);
+//         }
+//     })
+// })
+
+
