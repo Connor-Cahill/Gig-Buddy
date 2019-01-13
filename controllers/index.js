4c0cb684cbc8d4af5bcb0f3e53c376241ba6658f
Connor Cahill <connorcahill913@gmail.com>
2019-01-12 21:20:46 -0800

    added button when logged in on homepage

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,6 @@ module.exports = function(app) {
     //  GET: renders the landing page 
     app.get('/', (req, res) => {
         req.indexPage = true;
-        res.render('index', { index: req.indexPage });
+        res.render('index', { index: req.indexPage, user: req.user });
     });
 }
