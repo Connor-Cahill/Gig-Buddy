5a29298415e50872c526376739573a2a5b7dd7da
connor cahill <connorcahill913@gmail.com>
2019-04-16 18:39:27 +0000

    [Updates]: cookie name when checking for auth

--- middleware/checkForAuth.js
+++ middleware/checkForAuth.js
@@ -3,11 +3,11 @@ const jwt = require('jsonwebtoken');
 const wrap = require('./errorHandler');
 
 module.exports = wrap(async (req, res, next) => {
-    if (req.cookies.Token === undefined || req.cookies.Token === null) {
+    if (req.cookies.gigToken === undefined || req.cookies.gigToken === null) {
         req.user = null;
         return next();
     } else {
-        const token = req.cookies.Token;
+        const token = req.cookies.gigToken;
         const uid = jwt.decode(token, process.env.SECRET)._id;
         const user = await User.findById(uid).exec();
         req.user = user;
