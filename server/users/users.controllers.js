1dc5501bfbeeb163df9d01cf84ed951c42380972
connor cahill <connorcahill913@gmail.com>
2019-04-29 21:52:16 +0000

    [Refactors]: user controller to make more modular

--- server/users/users.controllers.js
+++ server/users/users.controllers.js
@@ -1,29 +1,39 @@
-const User = require('../models/user');
-const wrap = require('../middleware/errorHandler');
-const userAuth = require('../middleware/userAuth');
+const User = require('./users.model');
 
 
-module.exports = function(app) {
-    // GET: returns JSON list of all users
-    app.get('/api/users', userAuth, wrap(async (req, res) => {
-        const users = await User.find({}).exec();
-        return res.send(users);
-    }));
+//TODO: pass userAuth as middleware
+/**
+ * Index returns array of json user objects
+ */
+const Index = async (req, res) => {
+    const users = await User.find({}).exec();
+    return res.send(users);
+}
 
-    //  GET: Renders the users profile page 
-    app.get('/users/:id', userAuth, wrap(async (req, res) => {
-        const user = await User.findById(req.params.id).populate('services').populate('clients').exec();
-        return res.render('user-profile', { user });
-    }));
+// TODO: pass userAuth
+/**
+ * Get renders profile for specific users
+ */
+const Get = async (req, res) => {
+    const user = await User.findById(req.params.id)
+        .populate('services')
+        .populate('clients')
+        .exec();
+    // render the users profile
+    return res.render('user-profile', { user });
+}
 
-    //  PUT: edits user information 
-    app.put('/users/:id', userAuth, wrap(async (req, res) => {
-        const user = await User.findById(req.params.id).exec();
-        user.set(req.body);
-        await user.save();
-        //  Req is AJAX call so we just return a status 200
-        return res.sendStatus(200);
-    }));
+// TODO: pass userAuth as middleware
+/**
+ * Update changes and saves new info to user
+ */
+const Update = async (req, res) => {
+    const user = await User.findById(req.params.id).exec();
+    // set new information
+    user.set(req.body);
+    await user.save();
+    return res.sendStatus(200);
+}
 
     //  DELETE: deletes a user from the database
     app.delete('/users/:id', userAuth, wrap(async (req, res) => {
@@ -31,4 +41,21 @@ module.exports = function(app) {
         //  Don't know if this will render anything so it just returns 200
         return res.sendStatus(200);
     }));
+
+// TODO: pass userAuth middleware
+/**
+ * Delete removes a user from the database
+ */
+const Delete = async (req, res) => {
+    await User.findOneAndRemove({ _id: req.params.id }).exec();
+    return res.sendStatus(200);
+}
+
+
+module.exports = {
+    Index,
+    Get,
+    Create,
+    Update,
+    Delete,
 }
