8bad793717b70404ebeea09dc2f61bfc49a80d9d
connor cahill <connorcahill913@gmail.com>
2019-04-29 20:50:07 +0000

    [Adds]: routes to call controller functions

---
+++ server/payments/payments.routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const wrap = require('../middleware/errorHandler');
+const headerData = require('../middleware/calcHeaderStats');
+const sendCli = require('../middleware/sendClientsList');
+const userAuth = require('../middleware/userAuth');
+
+const router = express.Router();
+
+// GET: gets payments index which returns all payments
+router.get('/', userAuth, sendCli, headerData, wrap(controller.Index));
+
+// POST: creates a new payment
+router.post('/clients/:clientId', userAuth, wrap(controller.Create));
+
+// DELETE: removes payment from client and db
+router.delete('/:id/clients/:clientId', userAuth, wrap(controller.Delete));
+
+// PATCH: updates information about specific payment 
+router.patch('/:id', userAuth, wrap(controller.Update));
+
+// PATCH: closes a payment when client pays 
+router.patch('/:id/close', userAuth, wrap(controller.ClosePayment));
+
+module.exports = router;
