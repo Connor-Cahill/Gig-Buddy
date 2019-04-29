5d5966e8c801d2888e0b4d9c4e294738041ebc12
connor cahill <connorcahill913@gmail.com>
2019-04-29 20:07:24 +0000

    [Refactors]: controller putting all routes as functions

--- server/clients/clients.controllers.js
+++ server/clients/clients.controllers.js
@@ -1,127 +1,160 @@
 const Client = require('../models/client');
 const Service = require('../models/service');
 const User = require('../models/user');
-const wrap = require('../middleware/errorHandler');
 const headerData = require('../middleware/calcHeaderStats');
 const sendCli = require('../middleware/sendClientsList');
 const userAuth = require('../middleware/userAuth');
 const emailer = require('../services/sendgrid-emailer');
 
-module.exports = function(app) {
 
-    // // GET: returns list of all clients
-    app.get('/clients', userAuth, headerData, wrap( async (req, res) => {
-        //below vars are necessary for sending the data displayed in header
-        const totalServices = req.totalServices;
-        const monthlyServices = req.monthlyServices;
-        const oneTimeServices = req.oneTimeServices;
-        const totalClients = req.totalClients;
-        const totalEarned = req.totalEarned;
-        //  setting req.clientIndex for styling purposes
+/**
+ * Index returns list of all clients 
+ * with populated list of services for each
+ */
+const Index = async (req, res) => {
+    //below vars are necessary for sending the data displayed in header
+    const totalServices = req.totalServices;
+    const monthlyServices = req.monthlyServices;
+    const oneTimeServices = req.oneTimeServices;
+    const totalClients = req.totalClients;
+    const totalEarned = req.totalEarned;
+    // setting req.clientIndex for styling purposes
+    req.clientIndex = true;
+    const user = await User.findOne({ _id: req.user._id }).populate('clients').populate({
+        path: 'clients',
+        populate: {
+            path: 'services',
+        }
+    }).exec();
 
-        req.clientIndex = true;
+    // grab the client list from user
+    const clients = user.clients;
 
-        const user = await User.findOne({ _id: req.user._id }).populate('clients').populate({
-            path: 'clients',
-            populate: {
-                path: 'services'
-            }
-        }).exec();
-        const clients = user.clients;
-        res.render('clients-index', { clients, totalServices, totalEarned, monthlyServices, oneTimeServices, totalClients, clientIndex: req.clientIndex, user: req.user });
-    }));
+    return res.render('clients-index', { clients, totalServices, totalEarned, monthlyServices, oneTimeServices, totalClients, clientIndex: req.clientIndex, user: req.user });
+}
 
-    //  GET: renders the clients form and sends service data 
-    app.get('/clients/new', userAuth, wrap(async (req, res) => {
-        const user = await User.findOne({ _id: req.user._id }).populate('services').exec();
-        const services = user.services;
-        res.render('client-form', { services });
-    }));
+/**
+ * GetForm renders the new client form
+ * with list of services services to add to client
+ */
+const GetForm = async (req, res) => {
+    const user = await User.findOne({ _id: req.user._id }).populate('services').exec();
+    return res.render('client-form', { services: user.services });
+}
 
-    //  GET: returns single client given the ID 
-    app.get('/clients/:id', userAuth, headerData, sendCli, wrap( async (req, res) => {
-        //below vars are necessary for sending the data displayed in header
-        const totalServices = req.totalServices;
-        const monthlyServices = req.monthlyServices;
-        const oneTimeServices = req.oneTimeServices;
-        const totalClients = req.totalClients
-        const clients = req.clientList;
-        const totalEarned = req.totalEarned;
+/**
+ * Get returns information for single client with 
+ * populated and payment history/info
+ */
+const Get = async (req, res) => {
+    //below vars are necessary for sending the data displayed in header
+    const totalServices = req.totalServices;
+    const monthlyServices = req.monthlyServices;
+    const oneTimeServices = req.oneTimeServices;
+    const totalClients = req.totalClients
+    const clients = req.clientList;
+    const totalEarned = req.totalEarned;
 
-        
+    // get the client
+    const client = await Client.findOne({ _id: req.params.id })
+        .populate('services')
+        .populate('openPayments')
+        .exec();
 
-        const client = await Client.findOne({ _id: req.params.id }).populate('services').populate('billedServices').populate('openPayments').exec();
-        const user = await User.findById(req.user._id).populate('services').exec();
-        const services = user.services;
-        //  getting data for client snapshot
-        let totalOwes = 0;
-        client.openPayments.forEach((payment) => {
-            totalOwes += payment.amount;
-        });
-        const servicesCompleted = client.billedServices.length || 0;
-        
+    // get current user with services (so user can add services to client)
+    const user = await User.findById(req.user._id).populate('services').exec();
+    //  getting data for client snapshot
+    let totalOwes = 0;
+    client.openPayments.forEach((payment) => {
+        totalOwes += payment.amount;
+    });
+    const servicesCompleted = client.billedServices.length || 0;
+    return res.render('clients-show', { client, totalOwes, servicesCompleted, totalEarned, services, clients, totalServices, totalClients, monthlyServices, oneTimeServices, user });
+}
 
+/**
+ * Create makes a new client under a certain user
+ * saves to user and as new client in database
+ */
+const Create = async (req, res) => {
+    const client = new Client(req.body);
+    await client.save();
+    // save to user
+    const user = await User.findById(req.user._id).exec();
+    user.clients.unshift(client._id);
+    await user.save();
+    return res.redirect(`/clients/${client._id}`);
+}
 
+/**
+ * Update edits clients information and saves
+ * changes in database
+ */
+const Update = async (req, res) => {
+    const client = await Client.findById(req.params.id).exec();
+    // set new info
+    client.set(req.body);
+    await client.save();
+    return res.redirect(`/clients/${client._id}`);
+}
 
+/**
+ * Delete removes a client from a database and users list of clients
+ */
+const Delete = async (req, res) => {
+    await Client.findOneAndRemove({ _id: req.params.id }).exec(); 
+    // get user and remove client from clients list
+    const user = User.findById(req.user._id).exec();
+    user.clients.pop(user.clients.indexOf(req.params.id));
+    await user.save();
+    return res.redirect('/clients');
+}
 
-        res.render('clients-show', { client, totalOwes, servicesCompleted, totalEarned, services, clients, totalServices, totalClients, monthlyServices, oneTimeServices, user });
-    }));
+/**
+ * AddService adds new services to a clientj
+ */
+const AddService = async (req, res) => {
+    const client = await Client.findById(req.params.id).exec();
+    // grab services too add from request
+    const services = req.body.services;
+    // see if services is single value or array of services
+    (services.contructor === Array)
+        ? services.forEach(service => client.services.unshift(service))
+        : client.services.unshift(service);
+    await client.save();
+    return res.redirect(`/clients/${client._id}`);
+}
 
-    //  POST: creates a new client
-    app.post('/clients', userAuth, wrap( async (req, res) => {
-        const client = new Client(req.body);
-        await client.save();
-        const user = await User.findById(req.user._id).exec();
-        user.clients.unshift(client._id);
-        await user.save();
-        res.redirect(`/clients/${client._id}`);
-    }));
+/**
+ * RemoveService removes specific service from a client
+ */
+const RemoveService = async (req, res) => {
+    const client = await Client.findById(req.params.id).exec();
+    const service = await Service.findById(req.params.serviceId).exec();
+    // remove service from clients service list
+    client.service.pop(client.services.indexOf(service))
+    return res.sendSatus(200);
+}
 
-    // put: edits a client and updates it 
-    app.put('/clients/:id', userAuth, wrap(async (req, res) => {
-        const client = await Client.findOne({ _id: req.params.id }).exec();
-        client.set(req.body);
-        await client.save();
-        res.redirect(`/clients/${client._id}`);
-    }));
+/**
+ * SendMail allows user to send an email invoice to clients
+ */
+const SendMail = async (req, res) => {
+    const client = await Client.findById(req.params.id);
+    // get the users and clients email
+    const userEmail = req.user.email;
+    const clientEmail = client.email;
+    return emailer(userEmail, clientEmail, someTemplate)
+}
 
-    // DELETE: removes a client
-    app.delete('/clients/:id', userAuth, wrap(async (req, res) => {
-        await Client.findOneAndRemove({ _id: req.params.id }).exec();
-        const user = await User.findOne({ _id: req.user._id }).exec();
-        user.clients.pop(user.clients.indexOf(req.params.id));
-        await user.save();
-        res.redirect('/clients');
-    }));
-
-    //  PUT: adds services to client 
-    app.put('/clients/:id/addService', userAuth, wrap(async (req, res) => {
-        const client = await Client.findOne({ _id: req.params.id }).exec();
-        const services = req.body.services;
-
-        // See if services is an array or single 
-        ((services.constructor === Array) ? services.forEach(function(service) {
-            client.services.unshift(service);
-        }) : client.services.unshift(services));
-
-        await client.save();
-        res.redirect(`/clients/${client._id}`);
-    }));
-
-    // PUT: remove a service from a client
-    app.put('/clients/:id/service/:serviceId', userAuth, wrap(async (req, res) => {
-        const client = await Client.findOne({ _id: req.params.id }).exec();
-        const service = await Service.findOne({ _id: req.params.serviceId }).exec();
-        client.services.pop(client.services.indexOf(service));
-    }));
-
-    //POST: Route allows users to send emails to clients
-    // app.post('/clients/:id/email', userAuth, wrap(async (req, res) => {
-    //     const client = await Client.findById(req.params.id);
-    //     const userEmail = req.user.email;
-    //     const clientEmail = client.email;
-    //     emailer(userEmail, clientEmail, 'someTemplate')
-    // }));
-
-
+module.exports = {
+    Index,
+    Get,
+    Create,
+    Delete,
+    Update,
+    RemoveService,
+    AddService,
+    SendMail,
+    GetForm,
 }
