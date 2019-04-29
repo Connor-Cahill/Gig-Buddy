58ad35099990168983bdfce17af9581ae1c4b09e
connor cahill <connorcahill913@gmail.com>
2019-04-29 18:58:07 +0000

    [Adds]: implementation for controller functions

--- server/auth/auth.routes.js
+++ server/auth/auth.routes.js
@@ -5,10 +5,17 @@ const wrap = require('../../middleware/errorHandler');
 const router = express.Router();
 
 // POST: route signs up a user and issues a cookie
-router.post('/sign-up', controller.SignUp);
+router.post('/sign-up', wrap(controller.SignUp));
 
 // POST: route signs in existing user
-router.post('/sign-in', controller.SignIn);
+router.post('/sign-in', wrap(controller.SignIn));
 
+// GET: signs a user out removing jwt token
+router.get('/sign-out', wrap(controller.SignOut));
 
+// GET: renders the sign up page
+router.get('/sign-up', controller.GetSignUp);
+
+// GET: renders the sign in page
+router.get('/sign-in', controller.GetSignIn);
 
