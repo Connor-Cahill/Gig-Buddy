155193a0bd9e4846df26d81df6843f7ed98d69f2
connor cahill <connorcahill913@gmail.com>
2019-04-29 18:57:51 +0000

    [Refactors]: auth controller to implement exportable functions

--- server/auth/auth.controllers.js
+++ server/auth/auth.controllers.js
@@ -1,50 +1,72 @@
-const User = require('../models/user');
+const User = require('../users/users.model');
 const jwt = require('jsonwebtoken');
-const wrap = require('../middleware/errorHandler');
-
-module.exports = function(app) {
-
-    //GET: renders sign-up page
-    app.get('/sign-up', (req, res) => {
-        res.render('sign-up');
-    })
-
-    //GET: renders sign-in page
-    app.get('/sign-in', (req, res) => {
-        res.render('sign-in');
-    })
-
-    //GET (SIGN OUT): signs out the user and clears the cookies 
-    app.get('/sign-out', (req, res) => {
-        res.clearCookie(process.env.COOKIE);
-        res.redirect('/');
-        
-    })
-
-    //POST: Creates a new user when they sign up 
-    app.post('/sign-up', wrap(async (req, res) => {
-        const user = new User(req.body);
-        user.password = user.generateHash(req.body.password);
-        await user.save();
-        const token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: '60 days' });
-        res.cookie(process.env.COOKIE, token, { maxAge: 60 * 60 * 24 * 1000, httpOnly: true }); // maxAge set to 24 hours 
-        res.redirect('/clients');
-    }));
 
 
-    //POST: Signs user in if credentials are correct
-    app.post('/sign-in', wrap(async (req, res) => {
-        const user = await User.findOne({ email: req.body.email }, 'email password').exec();
-        if(!user) {
-            console.log('No account is registered with this email')
-            res.sendStatus(401);
-        } else if (!user.comparePassword(req.body.password)) {
-            console.log('Password is incorrect')
-            res.sendStatus(401);
-        } else {
-            const token = jwt.sign({_id: user._id}, process.env.SECRET, {expiresIn: '60 days'});
-            res.cookie(process.env.COOKIE, token, {maxAge: 24 * 60 * 60 * 1000, httpOnly: true}); //maxAge = 24 hours
-            res.redirect('/clients');
-        }
-    }));
+/**
+ * GetSignUp renders the sign up page view
+ */
+const GetSignUp = (req, res) => res.render('sign-up')
+
+
+/**
+ * GetSignIn renders the sign in page view
+ */
+const GetSignIn = (req, res) => res.render('sign-in')
+
+/**
+ * SignOut signs out a user by removing cookies
+ * redirects user to home page after
+ */
+const SignOut = (req, res) => {
+    res.clearCookie(process.env.COOKIE);
+    return res.redirect('/');
+}
+
+/**
+ * SignUp creates a new user with credentials
+ * adds to database and returns a jwt token
+ */
+const SignUp = async (req, res) => {
+    const user = new User(req.body);
+    // encrypts password
+    user.password = user.generateHash(req.body.password);
+    await user.save();
+    // returns token for user
+    return IssueToken(user, res);
+}
+
+/**
+ * SignIn checks if correct credentials and signs in
+ * existing user which results in returning token
+ */
+const SignIn = async (req, res) => {
+    const User = await User.findOne({ email: req.body.email }).exec();
+    // check if there is a user with that email
+    if (!user) {
+        console.log('No users match inputted credentials');
+        res.sendStatus(401);  
+    } else if (!user.comparePassword(req.body.password)) {
+        console.log('Incorrect credentials');
+        res.sendStatus(401);
+    } else {
+        return IssueToken(user, res);
+    }
+}
+
+/**
+ * IssueToken is called once user is authenitcated and
+ * returns a JWT token so user is authorized
+ */
+const IssueToken = (user, res) => {
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: '60 days' });
+    res.cookie(process.env.COOKIE, token, { maxAge: 60 * 60 * 24 * 1000, httpOnly: true }); // maxAge set to 24 hours 
+    return res.redirect('/clients');
+}
+
+module.exports = {
+    SignIn,
+    SignOut,
+    SignUp,
+    GetSignIn,
+    GetSignUp,
 }
